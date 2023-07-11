4c60fcc4083d861022085e10905c5cef4084dbc0
Thien Nguyen <s3426515@rmit.edu.vn>
2023-07-12 01:49:32 +1000

    Add reusable constants

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,25 @@
+
+const PHONE_NUMBERS = 'phoneNumbers';
+const FRANCHISEES = 'franchisees';
+const FRANCHISEE_SS = 'franchiseeSavedSearches';
+const OPERATORS = 'operators';
+const OPERATORS_BY_FRANCHISEE = 'operatorsByFranchisees';
+let tabNames = {};
+tabNames[PHONE_NUMBERS] = 'tab-phone-number'
+tabNames[FRANCHISEES] = 'tab-franchisees'
+tabNames[FRANCHISEE_SS] = 'tab-franchisees'
+tabNames[OPERATORS] = 'tab-operators'
+tabNames[OPERATORS_BY_FRANCHISEE] = 'tab-operators'
+
+export const VARS = {
+    PHONE_NUMBERS,
+    FRANCHISEES,
+    FRANCHISEE_SS,
+    OPERATORS,
+    OPERATORS_BY_FRANCHISEE,
+    tabNames,
+}
+
 export function debounce(fn, wait){
     let timer;
     return function(...args){
